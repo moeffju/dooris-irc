d361bce9cf23e277582974d8618e912a2fefeb27
Felix Favre <github@gnomus.de>
2014-01-19 23:29:20 +0100

    Added Wiki Feed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var config = require('./config.js');
 var cronjobs = require('./lib/cronjobs.js');
-var ircbot = require('./lib/ircbot.js');
+//var ircbot = require('./lib/ircbot.js');
 
 
 //Start all cronjobs

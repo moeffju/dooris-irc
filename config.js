3e2190a8dafd42acc227fc2415f79da2078ca9ff
Felix Favre <github@gnomus.de>
2014-01-21 20:31:11 +0100

    config reformat

--- config.js
+++ config.js
@@ -1,18 +1,22 @@
 module.exports = {
-	irc: {
-		server: "irc.hackint.org",
-		port: 9999,
-		sslCert: "hackint.pem",
-		channels: ["#ccchh"],
-		nick: "nodedooris"
-	},
-	doorstatus: {
-		url: "http://hamburg.ccc.de/dooris/dooris.json",
-		cronString: "0 * * * * *",
-		staticLeases: 2
-	},
-	wiki: {
-		url: "http://wiki.hamburg.ccc.de/index.php?title=Special:RecentChanges&feed=atom",
-		cronString: "30 * * * * *"
-	}
+  irc: {
+    server: "irc.hackint.org",
+    port: 9999,
+    sslCert: "hackint.pem",
+    channels: ["#ccchh"],
+    nick: "nodedooris"
+  },
+  doorstatus: {
+    url: "http://hamburg.ccc.de/dooris/dooris.json",
+    cronString: "0 * * * * *",
+    staticLeases: 2
+  },
+  wiki: {
+    url: "http://wiki.hamburg.ccc.de/index.php?title=Special:RecentChanges&feed=atom",
+    cronString: "30 * * * * *"
+  },
+  calendar: {
+    url: "http://wiki.hamburg.ccc.de/calendar.ics",
+    cronString: "*/10 * * * * *"
+  }
 };

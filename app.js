0bbbc6bd57aeaf22479a951ea9b83b55751a5266
Felix Favre <kontakt@gnomus.de>
2014-05-14 21:02:35 +0200

    added door open notification

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var config = require('./config.js');
 var cronjobs = require('./lib/cronjobs.js');
-//var ircbot = require('./lib/ircbot.js');
-
+var ircbot = require('./lib/ircbot.js');
+var express = require('express');
 
 //Start all cronjobs
 cronjobs.forEach(function(cron) {
   cron.start();
 });
+
+var webapp = express();
+
+webapp.get('/dooropen', function(req, res) {
+  ircbot.dooropen();
+  res.send("");
+});
+
+webapp.listen(3000);

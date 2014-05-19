8e053c10fa15e55267648f5f722274c592fd8dda
Felix Favre <kontakt@gnomus.de>
2014-05-19 22:30:19 +0200

    added doorstatus support for z9

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var config = require('./config.js');
 var cronjobs = require('./lib/cronjobs.js');
 var ircbot = require('./lib/ircbot.js');
 var express = require('express');
+var doorstatus = require('./lib/doorstatus.js');
+
 
 //Start all cronjobs
 cronjobs.forEach(function(cron) {
@@ -14,5 +16,25 @@ webapp.get('/dooropen', function(req, res) {
   ircbot.dooropen();
   res.send("");
 });
+
+webapp.get('/unlock', function(req, res) {
+  doorstatus.update(0);
+  config.irc.channels.forEach(function(chan) {
+    ircbot.updateTopic(chan, null);
+  });
+  res.send("");
+});
+
+webapp.get('/lock', function(req, res) {
+  doorstatus.update(1);
+  config.irc.channels.forEach(function(chan) {
+    ircbot.updateTopic(chan, null);
+  });
+  res.send("");
+});
+
+
+
+
 
 webapp.listen(3000);
